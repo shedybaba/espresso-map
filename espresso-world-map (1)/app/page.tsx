895615008fcf3d6ecae45c014d5026e2a7798568
shedybaba <70286717+shedybaba@users.noreply.github.com>
2025-09-15 16:50:48 +0100

    Delete espresso-world-map (1) directory

--- espresso-world-map (1)/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import { EspressoWorldMap } from "@/components/espresso-world-map"
-
-export default function Home() {
-  return (
-    <main className="min-h-screen">
-      <EspressoWorldMap />
-    </main>
-  )
-}
