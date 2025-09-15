1f0c94d5a98e999135c5289bcf053f61f44e51d6
shedybaba <70286717+shedybaba@users.noreply.github.com>
2025-09-15 16:57:49 +0100

    version 2 upload

---
+++ espresso-world-map (2)/app/page.tsx
@@ -0,0 +1,9 @@
+import { EspressoWorldMap } from "@/components/espresso-world-map"
+
+export default function Home() {
+  return (
+    <main className="min-h-screen">
+      <EspressoWorldMap />
+    </main>
+  )
+}
